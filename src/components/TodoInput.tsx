22e8f55cc49259a7d2a384ed87de3b79df2e025c
Administrator <admin@example.com>
2021-05-26 22:16:30 +0900

    Todo追加用インプットコンポーネント追加

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
-import { Stack, Input } from '@chakra-ui/react'
+import React, { useState, ChangeEvent, FormEvent } from 'react'
+import { Box, Input, Button, HStack } from '@chakra-ui/react'
+import { AddIcon } from '@chakra-ui/icons'
 
 const TodoInput: React.FC = () => {
+  const [value, setValue] = useState('')
+
+  const addTodo = (e: FormEvent<HTMLFormElement>) => {
+    if (value === '') return
+    e.preventDefault()
+    console.log(value)
+    setValue('')
+  }
+
   return (
-    <Stack spacing={3}>
-      <Input w="600px" variant="flushed" placeholder="Todo" />
-    </Stack>
+    <Box paddingTop="32px">
+      <form onSubmit={(e) => addTodo(e)}>
+        <HStack spacing="16px">
+          <Input
+            type="text"
+            width="640px"
+            color="white"
+            variant="flushed"
+            placeholder="Todo"
+            size="lg"
+            value={value}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setValue(e.target.value)
+            }
+          />
+          <Button type="submit">
+            <AddIcon />
+          </Button>
+        </HStack>
+      </form>
+    </Box>
   )
 }
 
