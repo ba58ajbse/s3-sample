a48c7f9dad320416175fbd3efbf39a7ddc122ad9
Administrator <admin@example.com>
2021-06-16 21:34:02 +0900

    edit todo item style

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { DeleteIcon } from '@chakra-ui/icons'
-import { Checkbox, ListItem } from '@chakra-ui/react'
+import { ListItem } from '@chakra-ui/react'
 import { TodoType } from '../interfaces/types'
-import { deleteTodoAsync } from '../store/slices/todoSlice'
+import { deleteTodoAsync, updateTodoAsync } from '../store/slices/todoSlice'
+import styled from 'styled-components'
 
 type PropType = {
   todoItem: TodoType
@@ -11,18 +12,29 @@ type PropType = {
 const TodoItem: React.FC<PropType> = ({ todoItem }) => {
   const dispatch = useDispatch()
 
-  const deleteTodo = async (id: string) => {
+  const deleteTodo = (id: string) => {
     dispatch(deleteTodoAsync(id))
   }
 
+  const toggleTodoChecked = (id: string, completed: boolean) => {
+    dispatch(updateTodoAsync(id, completed))
+  }
+
   return (
-    <ListItem marginBottom="12px" display="flex" position="relative">
-      <Checkbox marginRight="16px" iconSize="1.8" />
-      {todoItem.todo}
+    <ListItem h="24px" marginBottom="12px" display="flex" position="relative">
+      <StyledCheckbox
+        id={todoItem.id}
+        type="checkbox"
+        checked={todoItem.completed}
+        onChange={() => toggleTodoChecked(todoItem.id, todoItem.completed)}
+      />
+      <StyledLabel htmlFor={todoItem.id} completed={todoItem.completed}>
+        {todoItem.todo}
+      </StyledLabel>
       <DeleteIcon
         position="absolute"
+        top="6px"
         right="0"
-        bottom="0"
         cursor="pointer"
         onClick={() => deleteTodo(todoItem.id)}
       />
@@ -31,3 +43,12 @@ const TodoItem: React.FC<PropType> = ({ todoItem }) => {
 }
 
 export default TodoItem
+
+const StyledCheckbox = styled.input`
+  margin: 6px 16px 0 0;
+`
+
+const StyledLabel = styled.label<{ completed: boolean }>`
+  color: ${(props) => (props.completed ? 'gray' : 'inherit')};
+  text-decoration: ${(props) => (props.completed ? 'line-through' : 'none')};
+`
